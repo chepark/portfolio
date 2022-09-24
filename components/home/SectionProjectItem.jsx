44da3d4bf04477180f670d38551a9d7fbd60b56c
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-09-24 23:02:56 +0300

    Displayed the project title on hover.

--- components/home/SectionProjectItem.jsx
+++ components/home/SectionProjectItem.jsx
@@ -5,23 +5,21 @@ const SectionProjectItem = ({ project }) => {
   const imgDimensionClass =
     project.imageRatio == "landscape" ? "w-80 h-56" : "w-56 h-80";
 
-  const nextImageWidth = project.imageRatio == "landscape" ? 80 : 56;
-  const nextImageHeight = project.imageRatio == "landscape" ? 56 : 80;
+  const hoverAfterContentClass = `hover:after:content-['${project.title}']`;
 
   return (
-    <>
+    <div>
       <button
-        className={`${imgDimensionClass} bg-red-300 border-2 hover:drop-shadow-xl card-transform card-transition card-shadow`}
+        after={project.title}
+        className={`${imgDimensionClass} bg-red-300 border-2 hover:drop-shadow-xl card-transform card-transition card-shadow hover:after:content-[attr(after)] relative hover:after:absolute hover:after:-top-8 hover:after:left-12`}
       >
-        <div>
-          <Link href={project.url} passHref>
-            <a target="_blank">
-              <Image src={`/${project.id}.jpg`} layout="fill" />
-            </a>
-          </Link>
-        </div>
+        <Link href={project.url} passHref>
+          <a target="_blank">
+            <Image src={`/${project.id}.jpg`} layout="fill" />
+          </a>
+        </Link>
       </button>
-    </>
+    </div>
   );
 };
 
