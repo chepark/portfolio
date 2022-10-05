2cbb1fcac4cba03173999321d69cc871e1130814
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-10-05 18:41:06 +0300

    edited the Not Found message.

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>Not Found Page</div>;
+  return <div>Not Found Page. This page does not exist.</div>;
 };
 
 export default NotFound;
