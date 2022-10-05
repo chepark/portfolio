6431b443f33118dce4faa1f7f821a716a4df32a4
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-10-05 18:33:14 +0300

    organized code.

--- next.config.js
+++ next.config.js
@@ -3,18 +3,9 @@ const withMDX = require("@next/mdx")({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
   },
 });
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"],
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages (mdx) that depend on `fs` module
-  //   if (!isServer) {
-  //     config.resolve.fallback.fs = false;
-  //   }
-  //   return config;
-  // },
 });
