8fdcac4ae8e954da705d7c7c88fa7d518e788783
Chaeah Park <102865440+chepark@users.noreply.github.com>
2023-01-17 17:58:54 +0200

    Edited the project data.

--- components/home/SectionProjectList.jsx
+++ components/home/SectionProjectList.jsx
@@ -2,7 +2,8 @@ import { projectData } from "../../lib/projects";
 import ProjectList from "../projects/ProjectList";
 
 const SectionProjectList = () => {
-  const projects = projectData.slice(0, 7);
+  const projects = projectData.slice(0, 8);
+  console.log("project data:", projects);
   return <ProjectList projects={projects} />;
 };
 
