f4155d7afec4636397c13a251b6299ee57adf21f
Chaeah Park <chaeah.park@taiste.com>
2023-08-27 12:39:53 +0300

    Add type in PageHeader

--- components/pageHeader/PageHeader.tsx
+++ components/pageHeader/PageHeader.tsx
@@ -1,12 +1,22 @@
 import { useRouter } from "next/router";
 
-const PageHeader = ({ title }) => {
+type PageHeaderProps = {
+  title: string;
+  subTitle?: string;
+}
+
+const PageHeader = ({ title, subTitle }: PageHeaderProps) => {
   const { pathname } = useRouter();
   const textAlignClass = pathname == "/" ? "text-left" : "text-center";
 
   return (
     <header>
       <h1 className={`${textAlignClass} font-righteous`}>{title}</h1>
+      {pathname !== "" && (
+        <p className={`mt-3 text-base whitespace-pre-line ${textAlignClass}`}>
+          {subTitle}
+        </p>
+      )}
     </header>
   );
 };
